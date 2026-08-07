787815925b7ec3ce215fe623d459a7844b545157
jaish <singhjaishbiyen@gmail.com>
2026-08-07 19:44:20 +0100

    test cases added

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -43,6 +43,63 @@ mod test_preeti_converter {
     }
 
     #[test]
+    fn empty_string() {
+        assert_eq!(preeti_to_unicode("".to_owned()), "");
+    }
+
+    #[test]
+    fn already_unicode_passthrough() {
+        assert_eq!(preeti_to_unicode("नेपाल".to_owned()), "नेपाल");
+    }
+
+    #[test]
+    fn rupee_passthrough() {
+        assert_eq!(preeti_to_unicode("₹".to_owned()), "₹");
+    }
+
+    #[test]
+    fn digits() {
+        assert_eq!(preeti_to_unicode("!@#$%^&*()".to_owned()), "१२३४५६७८९०");
+    }
+
+    #[test]
+    fn reph_sanrakshan() {
+        assert_eq!(preeti_to_unicode(";+/If0f".to_owned()), "संरक्षण");
+    }
+
+    #[test]
+    fn nukta_ghazal() {
+        assert_eq!(preeti_to_unicode("uÞhÞn".to_owned()), "ग़ज़ल");
+    }
+
+    #[test]
+    fn nukta_faida() {
+        assert_eq!(preeti_to_unicode("kmÞfobf".to_owned()), "फ़ायदा");
+    }
+
+    #[test]
+    fn symbol_om() {
+        assert_eq!(preeti_to_unicode("ç".to_owned()), "ॐ");
+    }
+
+    #[test]
+    fn symbol_avagraha() {
+        assert_eq!(preeti_to_unicode("˜".to_owned()), "ऽ");
+    }
+
+    // "sIf" = क + क्ष् ; the trailing ् absorbs the following ा matra, so the
+    // standard converters (and this one) emit कक्ष (no explicit ा).
+    #[test]
+    fn mixed_digits() {
+        assert_eq!(preeti_to_unicode("sIf !)".to_owned()), "कक्ष १०");
+    }
+
+    #[test]
+    fn kram() {
+        assert_eq!(preeti_to_unicode("s|d".to_owned()), "क्रम");
+    }
+
+    #[test]
     fn test_html_entity() {
         let test_string = "ckg ;dfhs] sf]gf 3/d] a]6Ls] hgd x]nf;] ck;u'0f cf/ 3[0ff s/n hfo x}o . PsfO{;f} ztfAbLd] rn /xn ;dfh Pvlgof] 3/d] klxnsf af/] a]6f hGd e]nfk/ ef]h et]/ dgfjn hfo x} . jx] hux j]6Ls] hGd e]nf;] …3/d] nIdLÚ s] cfudg xf]on x}o sxn hfo x} n]lsg dgd] j]6f xj}s] OR5f bjfs] 5f]6df]6 ef]het]/ cf/ ljwL ljwfg s/n hfo x} . ha ls O ;dfhs] yfxf gxo gf/L lx O &gt;[i6Ls] gf/L xL rfnj x}o . ".to_string();
         let control = "अपन समाजके कोना घरमे बेटीके जनम हेलासे अपसगुण आर घृणा करल जाय हैय । एकाईसौ शताब्दीमे चल रहल समाज एखनियो घरमे पहिलका बारे बेटा जन्म भेलापर भोज भतेर मनावल जाय है । वहे जगह वेटीके जन्म भेलासे ‘घरमे लक्ष्मी’ के आगमन होयल हैय कहल जाय है लेकिन मनमे वेटा हवैके इच्छा दवाके छोटमोट भोजभतेर आर विधी विधान करल जाय है । जब कि इ समाजके थाहा नहय नारी हि इ श्रृष्टीके नारी ही चालव हैय । ".to_string();
@@ -139,13 +196,13 @@ mod test_unicode_converter {
         assert_eq!(converted, control);
     }
 
-    //#[test]
-    //fn temp_test2() {
-    //    let test_string = "क्रम".to_owned();
-    //    let control = "qmd".to_owned();
-    //    let converted = unicode_to_preeti(test_string);
-    //    assert_eq!(converted, control);
-    //}
+    #[test]
+    fn kram() {
+        let test_string = "क्रम".to_owned();
+        let control = "s|d".to_owned();
+        let converted = unicode_to_preeti(test_string);
+        assert_eq!(converted, control);
+    }
 
     #[test]
     fn temp_test3() {
@@ -177,5 +234,274 @@ mod test_unicode_converter {
         let control = "hfcf}+".to_owned();
         let converted = unicode_to_preeti(test_string);
         assert_eq!(converted, control);
+    }
+
+    #[test]
+    fn empty_string() {
+        assert_eq!(unicode_to_preeti("".to_owned()), "");
+    }
+
+    #[test]
+    fn html_entity() {
+        assert_eq!(unicode_to_preeti("&gt;&lt;".to_owned()), "><");
+        assert_eq!(unicode_to_preeti("&amp;".to_owned()), "&");
+    }
+
+    #[test]
+    fn english_passthrough() {
+        assert_eq!(unicode_to_preeti("Microwave".to_owned()), "Microwave");
+    }
+
+    #[test]
+    fn i_matra_simple() {
+        assert_eq!(unicode_to_preeti("कि".to_owned()), "ls");
+    }
+
+    #[test]
+    fn reph_only() {
+        assert_eq!(unicode_to_preeti("र्".to_owned()), "/\\");
+    }
+
+    #[test]
+    fn trailing_halant() {
+        assert_eq!(unicode_to_preeti("क्".to_owned()), "s\\");
+    }
+
+    #[test]
+    fn single_char() {
+        assert_eq!(unicode_to_preeti("क".to_owned()), "s");
+    }
+
+    #[test]
+    fn i_matra_alone() {
+        assert_eq!(unicode_to_preeti("ि".to_owned()), "l");
+    }
+
+    #[test]
+    fn digits() {
+        assert_eq!(
+            unicode_to_preeti("१२३४५६७८९०".to_owned()),
+            "!@#$%^&*()"
+        );
+    }
+
+    #[test]
+    fn prati() {
+        assert_eq!(unicode_to_preeti("प्रति".to_owned()), "k|lt");
+    }
+
+    #[test]
+    fn shree() {
+        assert_eq!(unicode_to_preeti("श्री".to_owned()), ">L");
+    }
+
+    #[test]
+    fn tribhuvan() {
+        assert_eq!(unicode_to_preeti("त्रिभुवन".to_owned()), "lqe'jg");
+    }
+
+    #[test]
+    fn kshama() {
+        assert_eq!(unicode_to_preeti("क्षमा".to_owned()), "Ifdf");
+    }
+
+    #[test]
+    fn gyan() {
+        assert_eq!(unicode_to_preeti("ज्ञान".to_owned()), "1fg");
+    }
+
+    #[test]
+    fn shraddha() {
+        assert_eq!(unicode_to_preeti("श्रद्धा".to_owned()), ">4f");
+    }
+
+    #[test]
+    fn shukrabar() {
+        assert_eq!(unicode_to_preeti("शुक्रबार".to_owned()), "z's|af/");
+    }
+
+    #[test]
+    fn hwatai() {
+        assert_eq!(unicode_to_preeti("ह्वातै".to_owned()), "Xjft}");
+    }
+
+    #[test]
+    fn uddeshya() {
+        assert_eq!(unicode_to_preeti("उद्देश्य".to_owned()), "pb\\b]Zo");
+    }
+
+    // BUG: unicode_to_preeti doubles the trailing vowel in the र्+vowel reph
+    // branch (src/lib.rs ~325/350 advances idx by 3 but emits the vowel, so it
+    // is emitted again). Correct Preeti is "b'of]{wg" (verified: that string
+    // round-trips to दुर्योधन). Our output "b'of]{f]wg" corrupts to दुर्याेेधन.
+    #[test]
+    #[ignore]
+    fn duryodhan() {
+        assert_eq!(unicode_to_preeti("दुर्योधन".to_owned()), "b'of]{wg");
+    }
+
+    // Known gap: unicode.json has no mapping for the nukta (़) back to Preeti's
+    // Þ. unicode_to_preeti currently leaves ़ raw in the output. Ignored until
+    // the map is fixed.
+    #[test]
+    #[ignore]
+    fn nukta_reverse() {
+        assert_eq!(unicode_to_preeti("ग़ज़ल".to_owned()), "uÞhÞn");
+    }
+}
+
+#[cfg(test)]
+mod test_normalise_unicode {
+    use crate::normalise_unicode;
+
+    #[test]
+    fn empty() {
+        assert_eq!(normalise_unicode("".to_owned()), "");
+    }
+
+    #[test]
+    fn single_char() {
+        assert_eq!(normalise_unicode("क".to_owned()), "क");
+    }
+
+    #[test]
+    fn trailing_halant() {
+        assert_eq!(normalise_unicode("क्".to_owned()), "क्");
+    }
+
+    #[test]
+    fn colon() {
+        assert_eq!(normalise_unicode(":".to_owned()), "M");
+    }
+
+    #[test]
+    fn colon_after_consonant() {
+        assert_eq!(normalise_unicode("स:".to_owned()), "सM");
+    }
+
+    #[test]
+    fn ta_ra() {
+        assert_eq!(normalise_unicode("त्र".to_owned()), "q");
+    }
+
+    #[test]
+    fn ka_ra() {
+        assert_eq!(normalise_unicode("क्र".to_owned()), "क|");
+    }
+
+    #[test]
+    fn retroflex_ra() {
+        assert_eq!(normalise_unicode("ट्र".to_owned()), "ट«");
+        assert_eq!(normalise_unicode("ठ्र".to_owned()), "ठ«");
+        assert_eq!(normalise_unicode("ड्र".to_owned()), "ड«");
+    }
+
+    #[test]
+    fn reph_passthrough() {
+        assert_eq!(normalise_unicode("र्म".to_owned()), "र्म");
+    }
+
+    #[test]
+    fn halant_before_purnabiram() {
+        assert_eq!(normalise_unicode("क्।".to_owned()), "क्।");
+        assert_eq!(normalise_unicode("क् ,".to_owned()), "क् ,");
+    }
+
+    #[test]
+    fn half_consonant_uppercase() {
+        assert_eq!(normalise_unicode("क्ष".to_owned()), "Sष");
+        assert_eq!(normalise_unicode("ह्व".to_owned()), "Xव");
+    }
+}
+
+#[cfg(test)]
+mod test_round_trip {
+    use crate::{preeti_to_unicode, unicode_to_preeti};
+
+    fn roundtrip(unicode: &str, preeti: &str) {
+        assert_eq!(unicode_to_preeti(unicode.to_owned()), preeti);
+        assert_eq!(preeti_to_unicode(preeti.to_owned()), unicode);
+    }
+
+    #[test]
+    fn truck() {
+        roundtrip("ट्रक", "6«s");
+    }
+
+    #[test]
+    fn khutta() {
+        roundtrip("खुट्टा", "v'6\\6f");
+    }
+
+    #[test]
+    fn swastha() {
+        roundtrip("स्वस्थ", ":j:y");
+    }
+
+    #[test]
+    fn sikista() {
+        roundtrip("सिकिस्त", "l;ls:t");
+    }
+
+    #[test]
+    fn jau_forms() {
+        roundtrip("जाऊ", "hfpm");
+        roundtrip("जाऊँ", "hfpmF");
+        roundtrip("जाऔं", "hfcf}+");
+    }
+
+    #[test]
+    fn gayau() {
+        roundtrip("गयौ?", "uof}<");
+    }
+
+    #[test]
+    fn prabhujile() {
+        roundtrip("प्रभुजीले", "k|e'hLn]");
+    }
+
+    #[test]
+    fn pahichan() {
+        roundtrip("पहिचान", "klxrfg");
+    }
+
+    #[test]
+    fn kshama() {
+        roundtrip("क्षमा", "Ifdf");
+    }
+
+    #[test]
+    fn gyan() {
+        roundtrip("ज्ञान", "1fg");
+    }
+
+    #[test]
+    fn shraddha() {
+        roundtrip("श्रद्धा", ">4f");
+    }
+
+    #[test]
+    fn shukrabar() {
+        roundtrip("शुक्रबार", "z's|af/");
+    }
+
+    #[test]
+    fn tribhuvan() {
+        roundtrip("त्रिभुवन", "lqe'jg");
+    }
+
+    #[test]
+    fn uddeshya() {
+        roundtrip("उद्देश्य", "pb\\b]Zo");
+    }
+
+    #[test]
+    fn kram() {
+        roundtrip("क्रम", "s|d");
+    }
+
+    #[test]
+    fn sanrakshan() {
+        roundtrip("संरक्षण", ";+/If0f");
     }
 }
